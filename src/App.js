4d8235ce2f089a91b65bd5288ea008dab6602e3b
Aman Kumar <kumar7080aman7080@gmail.com.com>
2022-11-08 20:35:53 +0530

    added react-redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className=" bg-slate-500">
-      <h1>Heading 1</h1>
-      <img src="/logo.png"></img>
+      <h1>Tailwind test</h1>
     </div>
   );
 }
