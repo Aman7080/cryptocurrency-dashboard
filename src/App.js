b121fdf7598a47444f91ba5efdd8a72074a7216f
Aman Kumar <kumar7080aman7080@gmail.com>
2022-12-05 20:28:23 +0530

    code refactor

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import "./App.css";
 import LogoContainer from "./components/LogoContainer";
 import Navbar from "./components/Navbar";
 import ChartCointainer from "./components/ChartCointainer";
-import PortfolioExchange from "./components/PortfolioExchange";
 import Coins from "./components/Coins";
+import PortfolioContainer from "./components/PortfolioContainer";
+import CoinExchange from "./components/CoinExchange";
 function App() {
   return (
     <>
       <LogoContainer />
-      <div className="flex m-10 h-full">
-        <div className="container  h-screen w-9/12 ">
-          <div className="">
+      <div className="flex flex-col md:flex-row m-10 h-full justify-center items-center">
+        <div className=" w-2/3">
+          <div>
             <Navbar />
             <ChartCointainer />
-            <PortfolioExchange />
+            <div className="flex space-x-4">
+              <PortfolioContainer />
+              <CoinExchange />
+            </div>
           </div>
         </div>
         <div className=" mx-5 bg-slate-50">
---
+++ src/components/CoinExchange.js
@@ -0,0 +1,62 @@
+import { BsFillCaretDownFill } from "react-icons/bs";
+
+function CoinExchange() {
+  return (
+    <div className="border bg-white rounded-xl p-5 w-full">
+      <div className="flex flex-col md:flex-row items-center m-5 justify-between">
+        <div>
+          <h3 className=" font-bold text-3xl ">Exchange Coins</h3>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex justify-around">
+          <h4 className="text-orange-600 text-xl">Sell</h4>
+          <div>
+            <select className=" bg-slate-50 rounded border appearance-none py-2 focus:outline-none focus:border-indigo-500 text-base pl-5 pr-14">
+              <option>Bitcoin</option>
+              <option>Polygon</option>
+              <option>Ethereum</option>
+              <option>Tron</option>
+            </select>
+            <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+              <BsFillCaretDownFill />
+            </span>
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Avl:0.002BTC"
+              class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8"
+            />
+          </div>
+        </div>
+        <div className="flex justify-around items-center pt-4 mt-5">
+          <h4 className="text-green-600 text-xl">Buy</h4>
+          <div className="relative">
+            <select className=" bg-slate-50 rounded border appearance-none py-2 focus:outline-none focus:border-indigo-500 text-base pl-5 pr-14">
+              <option>Bitcoin</option>
+              <option>Polygon</option>
+              <option>Ethereum</option>
+              <option>Tron</option>
+            </select>
+            <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+              <BsFillCaretDownFill />
+            </span>
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Avl:0.002BTC"
+              class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8"
+            />
+          </div>
+        </div>
+        <div className="flex justify-center pt-8">
+          <button className="bg-blue-600 py-2 px-3 rounded-md">Exchange</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CoinExchange;
