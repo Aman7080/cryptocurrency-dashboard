b8af1b2b239245bef3d6784dc4a4a7e9659d8d3b
Aman Kumar <kumar7080aman7080@gmail.com>
2022-12-07 13:12:10 +0530

    portfolio section finished

--- src/components/ChartCointainer.js
+++ src/components/ChartCointainer.js
@@ -1,31 +1,36 @@
-import React, { useState } from "react";
-// import BarChart from "./BarChart";
 import CartNavbar from "./ChartNavbar";
-import LineChart from "./LineChart";
-import { UserData } from "../DemoData";
-// import PieChart from "./PieChart";
+import { Line } from "react-chartjs-2";
+import { Price } from "../Data/cryptoPrice";
 
 export default function ChartCointainer() {
-
-
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
+  // data for chart
+  const userData = {
+    labels: Price[0].prices.map((time) => new Date(time[0]).toDateString()),
     datasets: [
       {
-        label: "Etherium",
-        data: UserData.map((data) => data.UserGain),
-        backgroundColor: ["red", "blue"],
+        label: "Ethereum",
+        data: Price[0].prices.map((price) => price[1]),
+        borderColor: "red",
+        pointRadius: "0",
       },
     ],
-  });
+  };
+  // option for removing x axis grid line
+  const options = {
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+  };
 
   return (
     <>
       <CartNavbar />
-      <div className="  bg-white rounded-md">
-        <LineChart chartData={userData} />
-        {/* <BarChart chartData={userData} /> */}
-        {/* <PieChart chartData={userData}/> */}
+      <div className=" bg-white rounded-md">
+        <Line data={userData} options={options} />
       </div>
     </>
   );
