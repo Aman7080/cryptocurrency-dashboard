e694cb9eb8584255a976606cd43225c138978a2c
Aman Kumar <kumar7080aman7080@gmail.com>
2022-12-07 14:01:06 +0530

    error fix

--- src/components/ChartCointainer.js
+++ src/components/ChartCointainer.js
@@ -1,13 +1,4 @@
 import CartNavbar from "./ChartNavbar";
-<<<<<<< HEAD
-import LineChart from "./LineChart";
-import { UserData } from "../DemoData"
-
-export default function ChartCointainer() {
-
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
-=======
 import { Line } from "react-chartjs-2";
 import { Price } from "../Data/cryptoPrice";
 
@@ -15,7 +6,6 @@ export default function ChartCointainer() {
   // data for chart
   const userData = {
     labels: Price[0].prices.map((time) => new Date(time[0]).toDateString()),
->>>>>>> b8af1b2b239245bef3d6784dc4a4a7e9659d8d3b
     datasets: [
       {
         label: "Ethereum",
@@ -39,14 +29,9 @@ export default function ChartCointainer() {
   return (
     <>
       <CartNavbar />
-<<<<<<< HEAD
-      <div className="  bg-white rounded-md">
-        <LineChart chartData={userData} />
-        {/* <BarChart chartData={userData} /> */}
-=======
+
       <div className=" bg-white rounded-md">
         <Line data={userData} options={options} />
->>>>>>> b8af1b2b239245bef3d6784dc4a4a7e9659d8d3b
       </div>
     </>
   );
