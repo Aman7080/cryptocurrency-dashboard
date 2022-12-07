0827e73f20c6d180277d51d3f822d6071796f774
Shivam-Saini-9 <strangboy999@gmail.com>
2022-12-07 14:19:04 +0530

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-import { BsSearch, BsFillCaretDownFill } from "react-icons/bs";
+import { BsFillCaretDownFill } from "react-icons/bs";
+import { FiSearch} from "react-icons/fi";
 
 export default function Navbar() {
   return (
@@ -16,8 +17,8 @@ export default function Navbar() {
         </span>
       </div>
       <div className="flex ml-3 flex-1 items-center rounded-lg bg-white h-14">
-        <div className=" ml-8 mr-5 text-4xl text-slate-500 ">
-          <BsSearch />
+        <div className=" ml-8 mr-5 text-3xl text-slate-500 ">
+          <FiSearch />
         </div>
         <input
           className=" h-11 w-full border-0 bg-transparent rounded-xl outline-0 pl-2 text-slate-600 focus:outline-none"
