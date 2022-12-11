7ccaeaa40e29c893950eea6d3a1533446f301b19
prabhkar singh <kprabhakar394@gmail.com>
2022-12-11 14:38:15 +0530

    serch bar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,77 @@
+import { useState } from "react";
 import { BsFillCaretDownFill } from "react-icons/bs";
 import { useSelector, useDispatch } from "react-redux";
 import { changeCurrency } from "../state/features/currency";
 import SearchBar from "./SearchBar";
-
+import { cryptocoins } from "../Data/cryptocoins";
+import { useEffect } from "react";
 export default function Navbar() {
   const dispatch = useDispatch();
   const currency = useSelector((state) => state.defaultCurrency)[0];
-
+  const [searchData, setSearchData] = useState([])
+  const [inputValue, setInputValue] = useState('')
   //const [currency, setCurrency] = useState("usd");
   // useEffect(() => dispatch(changeCurrency(currency)), [currency]);
+
+  const fillCompleteName = (name) => {
+    console.log('got name is',name);
+    setInputValue(name)
+    setSearchData([])
+    setTimeout(() => {
+      setSearchData([])
+    },0)
+  }
+
+  console.log('inputvalue is',inputValue);
+  useEffect(() => {
+    if (inputValue) {
+      let searchText = inputValue.toLowerCase()
+      // console.log('st is', searchText);
+
+      let filterData = cryptocoins.filter((crypto) => {
+        let cName = crypto?.name.toLowerCase();
+        if (cName.includes(searchText)) {
+          return true
+        }
+        return false
+      })
+      setSearchData(filterData)
+    }
+  }, [inputValue])
   return (
-    <div className="w-full max-sm:w-full  font-['Poppins'] flex justify-between my-4">
-      <div className="relative">
-        <select
-          value={currency}
-          onChange={(e) => dispatch(changeCurrency(e.target.value))}
-          className="h-14 max-sm:w-26 shadow-md hover:duration-300 hover:shadow-xl bg-white rounded-xl appearance-none py-2 focus:outline-none focus:border-indigo-500 text-gray-600 text-base pl-5 pr-14"
-        >
-          <option value="usd">USD</option>
-          <option value="inr">INR</option>
-        </select>
-        <span className="absolute right-0 top-0 h-full w-10 text-center text-black font-black pointer-events-none flex items-center justify-center">
-          <BsFillCaretDownFill />
-        </span>
+    <>
+      <div className="w-full max-sm:w-full  font-['Poppins'] flex justify-between my-4">
+        <div className="relative">
+          <select
+            value={currency}
+            onChange={(e) => dispatch(changeCurrency(e.target.value))}
+            className="h-14 max-sm:w-26 shadow-md hover:duration-300 hover:shadow-xl bg-white rounded-xl appearance-none py-2 focus:outline-none focus:border-indigo-500 text-gray-600 text-base pl-5 pr-14"
+          >
+            <option value="usd">USD</option>
+            <option value="inr">INR</option>
+          </select>
+          <span className="absolute right-0 top-0 h-full w-10 text-center text-black font-black pointer-events-none flex items-center justify-center">
+            <BsFillCaretDownFill />
+          </span>
+        </div>
+        <SearchBar inputValue={inputValue} setInputValue={setInputValue} />
+
       </div>
-      <SearchBar />
-    </div>
+      {searchData.length>0 && inputValue &&
+        <div className=" bg-white border absolute" style={{ width: '55%', marginLeft: '126px',zIndex: 1, marginTop: '-1rem' }}>
+          {
+            searchData.map((crypto, index) => {
+              return(
+                <div className=" each-search-data px-20" key={index} >
+                  <h1 className="my-2" onClick={() => {
+                    fillCompleteName(crypto?.name)
+                  }}>{crypto?.name}</h1>
+                </div>
+              )
+            })
+          }
+        </div>
+      }
+    </>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,48 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import { cryptocoins } from "../Data/cryptocoins";
+const SearchBar = ({inputValue, setInputValue}) => {
+    
+    // console.log('cry');
+    const searchHandler = (e) => {
+        // let searchText = e.target.value.toLowerCase();
+        // console.log('st is', searchText);
 
-const SearchBar = () => {
+        // let filterData = cryptocoins.filter((crypto) => {
+        //     let cName = crypto?.name.toLowerCase();
+        //     if (cName.includes(searchText)) {
+        //         return true
+        //     }
+        //     return false
+        // })
+        // console.log('filter data is', filterData);
+        setInputValue(e.target.value)
+    }
     return (
-        <div className="w-full flex ml-3 hover:duration-300 hover:shadow-xl shadow-md flex-1 items-center rounded-lg bg-white h-14">
-            <div className=" ml-8 mr-5 text-3xl text-slate-500 ">
-                <FiSearch />
+        <>
+            {/* <div> */}
+
+            <div className="w-full flex ml-3 hover:duration-300 hover:shadow-xl shadow-md flex-1 items-center rounded-lg bg-white h-14">
+                <div className=" ml-8 mr-5 text-3xl text-slate-500 ">
+                    <FiSearch />
+                </div>
+                <input
+                    className=" h-11 w-full border-0 bg-transparent rounded-xl outline-0 pl-2 text-slate-600 focus:outline-none"
+                    placeholder="Search by Coin"
+                    onChange={searchHandler}
+                    value={inputValue}
+                />
             </div>
-            <input
-                className=" h-11 w-full border-0 bg-transparent rounded-xl outline-0 pl-2 text-slate-600 focus:outline-none"
-                placeholder="Search by Coin"
-            />
-        </div>
+            {/* </div> */}
+
+            {/* <div className="w-full my-24" style={{height: '65px', backgroundColor: 'red' }}>
+                {
+                    searchData.map((item) => {
+                        <h5>{item?.name}</h5>
+                    })
+                }
+            </div> */}
+        </>
     )
 }
 
