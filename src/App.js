877aa2dceeaf3ac726d737368b6d1b68f3d6e200
prabhkar singh <kprabhakar394@gmail.com>
2023-03-01 17:01:50 +0530

    loading

--- src/App.js
+++ src/App.js
@@ -5,29 +5,57 @@ import ChartCointainer from "./components/ChartCointainer";
 import Coins from "./components/CoinsMktCap";
 import Portfolio from "./components/Portfolio";
 import CoinExchange from "./components/CoinExchange";
+import { useEffect, useState } from "react";
+import { BallTriangle } from "react-loader-spinner";
+
 function App() {
+  const [completed, setCompleted] = useState(undefined);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCompleted(true);
+    }, 2500);
+  });
+
   return (
     <>
-      <div className="dark:bg-stone-900 bg-neutral-50">
-        <LogoContainer />
-        <div className="flex dark:bg-stone-900 flex-col items-center md:items-start lg:flex-row justify-center m-10 max-[820px]:flex-col">
-          <div className="container max-w-screen-lg">
-            <div>
-              <Navbar />
+      {!completed ? (
+        <div className="w-screen h-[90vh] flex justify-center items-center ">
+          <BallTriangle
+            height={100}
+            width={100}
+            radius={5}
+            color="#4fa94d"
+            ariaLabel="ball-triangle-loading"
+            wrapperClass={{}}
+            wrapperStyle=""
+            visible={true}
+          />
+        </div>
+      ) : (
+        <>
+          <div className="dark:bg-stone-900 bg-neutral-50">
+            <LogoContainer />
+            <div className="flex dark:bg-stone-900 flex-col items-center md:items-start lg:flex-row justify-center m-10 max-[820px]:flex-col">
+              <div className="container max-w-screen-lg">
+                <div>
+                  <Navbar />
 
-              <ChartCointainer />
+                  <ChartCointainer />
 
-              <div className="flex flex-col md:flex-row md:space-x-4">
-                <Portfolio />
-                <CoinExchange />
+                  <div className="flex flex-col md:flex-row md:space-x-4">
+                    <Portfolio />
+                    <CoinExchange />
+                  </div>
+                </div>
+              </div>
+              <div className="max-[1022px]:flex-col max-[1022px]:w-full max-[1022px]:mx-0 mx-4 h-[83rem]   dark:bg-stone-900">
+                <Coins />
               </div>
             </div>
           </div>
-          <div className="max-[1022px]:flex-col max-[1022px]:w-full max-[1022px]:mx-0 mx-4 h-[83rem]   dark:bg-stone-900">
-            <Coins />
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 }
