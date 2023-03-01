4f50b02133dfef64dbaec941611af00a0c3691ae
prabhkar singh <kprabhakar394@gmail.com>
2023-03-01 13:44:19 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
 import "./App.css";
-// import { useEffect, useState, useSelector, useDispatch } from "react";
 import LogoContainer from "./components/LogoContainer";
 import Navbar from "./components/Navbar";
 import ChartCointainer from "./components/ChartCointainer";
 import Coins from "./components/CoinsMktCap";
 import Portfolio from "./components/Portfolio";
 import CoinExchange from "./components/CoinExchange";
-// import { BallTriangle } from "react-loader-spinner";
 function App() {
-  // const dispatch = useDispatch();
-  // const data = useSelector((state) => state.default);
-  // const [completed, setCompleted] = useState(undefined);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     if (data.coinList.length === 0) {
-  //       dispatch(fetchCoins());
-  //       setCompleted(true);
-  //     }
-  //   }, 2500);
-  // }, [data.coinList.length, dispatch]);
-
   return (
     <>
-      {/* {!completed ? (
-        <div className="w-screen h-[90vh] flex justify-center items-center ">
-          <BallTriangle
-            height={100}
-            width={100}
-            radius={5}
-            color="#4fa94d"
-            ariaLabel="ball-triangle-loading"
-            wrapperClass={{}}
-            wrapperStyle=""
-            visible={true}
-          />
-        </div>
-      ) : ( */}
-      {/* <> */}
       <div className="dark:bg-stone-900 bg-neutral-50">
         <LogoContainer />
         <div className="flex dark:bg-stone-900 flex-col items-center md:items-start lg:flex-row justify-center m-10 max-[820px]:flex-col">
@@ -58,8 +28,6 @@ function App() {
           </div>
         </div>
       </div>
-      {/* </>
-      )} */}
     </>
   );
 }
