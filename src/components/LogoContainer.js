678fa4a952d0186251c8d507f44b2c57a6736911
Shivam-Saini-9 <strangboy999@gmail.com>
2023-03-01 11:00:18 +0530

    u

--- src/components/LogoContainer.js
+++ src/components/LogoContainer.js
@@ -27,14 +27,14 @@ function LogoContainer() {
   return (
     <div className="dark:bg-stone-700 dark:outline-none dark:border-none flex justify-between shadow-md border ">
       <img
-        className=" h-14 my-3 bg-white dark:bg-stone-700 px-10 rounded-2xl "
+        className=" sm:h-14 h-10 my-3 bg-white dark:bg-stone-700 px-10 rounded-2xl "
         src="/logo.png"
         alt="logo"
       ></img>
 
       <button
         onClick={handleThemeSwitch}
-        className="bg-stone-900 dark:bg-stone-200 my-3 mr-16 font-poppins font-medium p-2 justify-around inline-flex rounded-full"
+        className="bg-stone-900 dark:bg-stone-200 my-3 mr-16 font-poppins font-normal sm:p-2 p-0 justify-around inline-flex rounded-full"
       >
         <span className="m-1 text-white dark:text-stone-900 text-4xl font-black">
             <CgDarkMode />
